2d04e0356264504c67187b29554bebadd6eac667
jnkriszti <joni.krisztinavivien@gmail.com>
2025-04-22 19:49:02 +0200

    bj javítás

--- bj-rps/vite.config.js
+++ bj-rps/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwind from "@";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwind(), react()],
+  plugins: [react()],
 });
